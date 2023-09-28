255e805556bc0c13373e1031f20b84b2624d4053
Azzam ALmasslamany <azzammeri24@gmail.com>
2023-09-28 15:39:17 +0300

    I Write The Full Code Here

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+console.log(add(2, 3)); // Output: 5
+
+
+const multiply = (a, b) => a * b;
+console.log(multiply(4, 5)); // Output: 20
+
+const greeting = () => {
+  return "Hello, world!";
+};
+console.log(greeting()); // Output: Hello, world!
